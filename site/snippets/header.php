18a8bb01b47c8557e06fe42b47f2e0082284f218
Anwar Choukah <anwarchoukah@gmail.com>
2014-08-07 09:13:14 +0100

    Blog index: Pagination and thumbnail added
    The image thumbnail is not showing but is styled appropriately.
    
    Pagination works beautifully, showing 10 articles per page

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -12,12 +12,11 @@
   <?php echo css('assets/styles/style.css') ?>
   <?php echo js('assets/js/jquery-1.11.0.min.js') ?>
   <?php echo js('assets/js/lightbox.min.js') ?>
+  <?php echo js('//www.google-analytics.com/analytics.js') ?>
 
   <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700,300italic|Playfair+Display:400,700,900,400italic,700italic,900italic|Inconsolata' rel='stylesheet' type='text/css'>
   
-  <link rel="alternate" type="application/rss+xml" href="<?php echo url('blog/feed') ?>" title="Blog Feed" />
-
-  <script async src="//www.google-analytics.com/analytics.js"></script>
+  <link rel="alternate" type="application/rss+xml" href="<?php echo url('blog/feed') ?>" title="<?php echo html($pages->find('blog/feed')->title()) ?>" />
 
 </head>
 
--- site/templates/blog.php
+++ site/templates/blog.php
@@ -6,14 +6,35 @@
   <!-- <h1><?php echo html($page->title()) ?></h1> -->
   <h2><?php echo kirbytext($page->text()) ?></h2>
   
-  <?php foreach($page->children()->visible()->flip() as $article): ?>
+  <?php $articles = $page->children()->visible()->flip()->paginate(10) ?>
+
+  <?php foreach($articles as $article): ?>
+
+
   
   <article class="blog-item">
     <h2><a href="<?php echo $article->url() ?>"><?php echo html($article->title()) ?></a></h2>
+    <img class="blog-thumb" src="01-thumb.png" alt="">
     <p><?php echo excerpt($article->text(), 300) ?></p>
+    <a href="<?php echo $article->url() ?>" class="read-more">Read more</a>
   </article>
 
   <?php endforeach ?>
+
+  <?php if($articles->pagination()->hasPages()): ?>
+    <nav class="pagination">  
+
+    <?php if($articles->pagination()->hasNextPage()): ?>
+    <a class="prev" href="<?php echo $articles->pagination()->nextPageURL() ?>">&lsaquo; Previous posts</a>
+    <?php endif ?>
+
+    <?php if($articles->pagination()->hasPrevPage()): ?>
+    <a class="next" href="<?php echo $articles->pagination()->prevPageURL() ?>">Next posts &rsaquo;</a>
+    <?php endif ?>
+
+  </nav>
+
+<?php endif ?>
 
 </section>
 
